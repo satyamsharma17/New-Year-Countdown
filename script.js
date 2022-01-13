4912b3ff0c71d4c2a92f1d449d65fce88683126d
satyamsharma17 <satyamsharma1725@gmail.com>
2022-01-14 02:14:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+let dayBox = document.getElementById("day-box");
+let hrBox = document.getElementById("hr-box");
+let minBox = document.getElementById("min-box");
+let secBox = document.getElementById("sec-box");
+
+// Format: Date (Year, Month, Day, Hour, Minute)
+// Year is counter from 0 to 11.
+
+let endDate = new Date(2022,12,31,23,59);
+
+// Output value in milliseconds.
+
+let endTime = endDate.getTime();
+
+function countdown(){
+    let todayDate = new Date();
+
+    // Output value in milliseconds.
+
+    let todayTime = todayDate.getTime();
+
+    let remainingTime = endTime - todayTime;
+
+    // 60 second = 1000 milliseconds.
+    let oneMin = 60 * 1000;
+    // 1 hour = 60 minutes.
+    let oneHr = 60 * oneMin;
+    // 1 day = 24 hours.
+    let oneDay = 24 * oneHr;
+
+    // Function to format number if it is single digit.
+
+    let addZeroes = num => num < 10 ? `0${num}` : num;
+
+    // If end date is before today date.
+
+    if(endTime < todayTime){
+        clearInterval(i);
+        document.querySelector(".countdown").innerHTML = `<h1>Countdown had expired!</h1>`;
+    }
+
+    // If end date is not before today date.
+    else{
+
+        // Calculating remaining days, hours, minutes ,seconds.
+        
+        let daysLeft = Math.floor(remainingTime / oneDay);
+        let hrsLeft = Math.floor((remainingTime % oneDay) / oneHr);
+        let minsLeft = Math.floor((remainingTime % oneHr) / oneMin);
+        let secsLeft = Math.floor((remainingTime % oneMin) / 1000);
+
+        // Displaying Values.
+
+        dayBox.textContent = addZeroes(daysLeft);
+        hrBox.textContent = addZeroes(hrsLeft);
+        minBox.textContent = addZeroes(minsLeft);
+        secBox.textContent = addZeroes(secsLeft);
+    }
+}
+let i = setInterval(countdown,1000);
+countdown();
